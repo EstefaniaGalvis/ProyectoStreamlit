b55219235439d21b7cd192df16bb167756a46a07
EstefaniaGalvis <estefaniagalvis27@gmail.com>
2024-06-07 23:05:05 -0500

    Nuevo orden de código en página uno

--- pages/1_Proyecto Integrador.py
+++ pages/1_Proyecto Integrador.py
@@ -2,19 +2,17 @@ import streamlit as st
 import pandas as pd
 from df import df
 
+st.title("Proyecto Integrador")
 #------------------------------------------------------------------------------
 
-st.header("Peliculas con mayor nota entre las fechas seleccionadas")
+def filtro1():
+    FechaInicial = st.date_input("Ingrese la fecha inicial",value=None)
 
-FechaInicial = st.date_input("Ingrese la fecha inicial",value=None)
-
-FechaFinal = st.date_input("Ingrese la fecha Final",value=None)
+    FechaFinal = st.date_input("Ingrese la fecha Final",value=None)
 
 #------------------------------------------------------------------------------
 
-st.header("Peliculas con mayor puntuación segun la categoría seleccionada")
-
-def categorias_peliculas():
+def filtro2():
     CategoriasP = df['Categoria'].unique()
     opcion = st.selectbox("Categorias", CategoriasP)
     opcion_seleccionada = df[df['Categoria']==opcion]
@@ -24,4 +22,19 @@ def categorias_peliculas():
     df_grafico = pd.DataFrame(data)
     st.bar_chart(df_grafico.set_index('Titulo'))
 
-categorias_peliculas()
+
+filtros =[
+    "Peliculas con mayor puntuación entre las fechas seleccionadas",
+    "Peliculas con mayor puntuación segun la categoría seleccionada",
+    ""
+]
+
+filtro = st.selectbox("Filtros",filtros)
+
+if filtro:
+    filtro_index = filtros.index(filtro)
+
+    if filtro_index == 0:
+        filtro1()
+    elif filtro_index == 1:
+        filtro2()
