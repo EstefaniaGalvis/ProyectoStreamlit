3508fd4ea2bad2ba3cf733f47873b065564a7c26
Santiago Hernandez <santiago224z90@gmail.com>
2024-06-15 08:25:13 -0500

    Ultimo filtro e imagenes

--- inicio.py
+++ inicio.py
@@ -1,24 +1,38 @@
 import streamlit as st
 
-st.title("Proyecto integrador")
+st.image("./static/images/logo.png", width=200)
+
+st.title("Proyecto integrador") 
 
 st.write("Hecho por: Santiago Hernandez Espejo, Juan Camilo Ossa, Estefania Galvis Guainas")
-
-st.header("Introducción a Replay: Portal de Entretenimiento")
-
-st.write("En un panorama de entretenimiento digital en constante evolución, presentamos Replay, una plataforma revolucionaria diseñada para ofrecerte una experiencia de streaming única y personalizada. Con un enfoque en la excelencia tecnológica y la satisfacción del usuario, Replay redefine la forma en que disfrutas del contenido audiovisual y en el corazón de Replay se encuentra una base de datos alojada en Firebase, que actúa como el motor de nuestro servicio.")
-st.write("Esta base de datos, alimentada por algoritmos inteligentes, permite una clasificación y organización meticulosa de un vasto catálogo de películas, series y documentales, brindándote acceso a una biblioteca diversa de contenido.")
 
 st.subheader("Principal objetivo")
 
 st.write("El principal objetivo de este proyecto de Streamlit es realizar diferentes filtros haciendo uso de Pandas y de la base de datos previamente realizada en Firebase")
 
-
 st.subheader("CaracterÍsticas y detalles")
 
 st.write("En las diferentes páginas se encuentras contextos y filtros diferentes, entre esas están:")
+
+#Proyecto Integrador
+
 st.write("1. PROYECTO INTEGRADOR: Esta página busca crear filtros para un dataset alojado en Firebase, con proyecto interador nos referimos a aquel proyecto del cual hemos trabajo en diferentes submodulos.")
 
-st.write("2. SIMULADOR CESDE: Esta página viene como proyecto especifico del submodulo de Nuevas Tecnologias de la Programación, apartir de una base de datos proporionada por el profesor se busca realizar diferentes filtros.")
+st.header("Introducción a Replay: Portal de Entretenimiento")
 
-st.write("3. TERCER PROYECTO: Esta última página consta con un dataset extraído directamente de Kaggle, una plataforma con una amplia comunidad dedicada a ciencias de datos, desde esta plataforma buscamos también crear diferentes filtros que ayude a la buqueda de datos específicos.")
+st.image("./static/images/integrador.png", width=400)
+
+st.write("En un panorama de entretenimiento digital en constante evolución, presentamos Replay, una plataforma revolucionaria diseñada para ofrecerte una experiencia de streaming única y personalizada. Con un enfoque en la excelencia tecnológica y la satisfacción del usuario, Replay redefine la forma en que disfrutas del contenido audiovisual y en el corazón de Replay se encuentra una base de datos alojada en Firebase, que actúa como el motor de nuestro servicio.")
+st.write("Esta base de datos, alimentada por algoritmos inteligentes, permite una clasificación y organización meticulosa de un vasto catálogo de películas, series y documentales, brindándote acceso a una biblioteca diversa de contenido.")
+
+#Simulador CESDE
+
+st.write("2. SIMULADOR CESDE: Esta página viene como proyecto especifico del submodulo de Nuevas Tecnologias de la Programación, apartir de una base de datos proporionada por el profesor se busca realizar diferentes filtros; esta base de datos consta de diferentes notas de estudiantes de un curso de programación.")
+
+st.image("./static/images/notas.png", width=400)
+
+#Ventas de una tienda online
+
+st.write("3. TERCER PROYECTO: Esta última página consta con un dataset extraído directamente de Kaggle, una plataforma con una amplia comunidad dedicada a ciencias de datos, desde esta plataforma buscamos también crear diferentes filtros que ayude a la buqueda de datos específicos; el dataset usado en este proyecto es de ventas de una tienda online.")
+
+st.image("./static/images/productos.png", width=400)
--- pages/1_Proyecto Integrador.py
+++ pages/1_Proyecto Integrador.py
@@ -7,6 +7,13 @@ df = pd.DataFrame(data)
 
 st.title("Proyecto Integrador")
 
+
+
+
+
+
+
+
 def filtro1():
     fecha_minima = df['FechaEstreno'].min()
     fecha_maxima = df['FechaEstreno'].max()
@@ -17,14 +24,15 @@ def filtro1():
     fecha_inicial_str = FechaInicial.strftime('%Y-%m-%d')
     fecha_final_str = FechaFinal.strftime('%Y-%m-%d')
 
-    opcion_seleccionada = df[(df['FechaEstreno'] >= fecha_inicial_str) & (df['FechaEstreno'] <= fecha_final_str)]
+    opcion_seleccionadaF = df[(df['FechaEstreno'] >= fecha_inicial_str) & (df['FechaEstreno'] <= fecha_final_str)]
 
-    st.table(opcion_seleccionada)
+    
 
-    data = opcion_seleccionada[['Titulo', 'Puntuacion']]
+    data = opcion_seleccionadaF[['Titulo', 'Puntuacion']]
     df_grafico = pd.DataFrame(data)
 
-    st.bar_chart(df_grafico.set_index('Titulo'))
+    st.bar_chart(df_grafico.head(30).set_index('Titulo'))
+    st.table(opcion_seleccionadaF.head(30))
 
 def filtro2():
     CategoriasP = df['Categoria'].unique()
@@ -40,13 +48,30 @@ def filtro2():
 
     st.bar_chart(df_grafico.set_index('Titulo'))
 
+
+def filtro3():
+    ActoresP = df['Actores'].unique()
+
+    opcion = st.multiselect("Actores", ActoresP)
+
+    opcion_seleccionada = df[df['Actores'].isin(opcion)]
+
+    data = opcion_seleccionada[['Titulo', 'Puntuacion']]
+    df_grafico = pd.DataFrame(data)
+
+    st.bar_chart(df_grafico.set_index('Titulo'))
+
+    st.table(opcion_seleccionada)
+
+# Filtros
 filtros = [
-    "Peliculas con mayor puntuación entre las fechas seleccionadas",
-    "Peliculas mostradas según la categoría seleccionada"
+    "Peliculas publicadas entre las fechas seleccionadas",
+    "Peliculas publicadas según la categoría seleccionada",
+    "Peliculas publicadas según el actor seleccionado"
 ]
 
 filtro = st.selectbox("Filtros", filtros)
-
+ 
 if filtro:
     filtro_index = filtros.index(filtro)
 
@@ -54,3 +79,5 @@ if filtro:
         filtro1()
     elif filtro_index == 1:
         filtro2()
+    elif filtro_index == 2:
+        filtro3()
